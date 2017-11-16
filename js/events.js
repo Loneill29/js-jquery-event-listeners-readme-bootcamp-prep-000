167d6fe39d0d4aee60746e726b217a318b6e7800
Linda ONeill <oneill.linda21@gmail.com>
2017-11-16 19:53:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
        alert("Hey!");
      });
    }
-   
+
    function frameIt() {
      $('img').on('load', function() {
        $('img').addClass('tasty');
      });
    }
-   
+
    function pressIt() {
      $("input#typing").on('keydown', function(key) {
        if (key.keyCode == 71) {
@@ -20,9 +20,9 @@ $(document).ready(function() {
        }
      });
    }
-    		  
+
    }); 		 +function submitIt() {
      $('form').on('submit', function() {
        alert("your form is going to be submitted now");
        return;
- +  });
+   });
